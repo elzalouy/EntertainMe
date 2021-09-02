3e64ca4798edf7b1edea5f6a8ef10d6b4976c6ee
ezat-elzalouy <ezatelzalouy711@gmail.com>
2021-09-02 12:47:00 +0200

    1.0.0

--- src/components/Home/Artists.jsx
+++ src/components/Home/Artists.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {Swiper,SwiperSlide} from 'swiper/react';
+import 'swiper/css';
 import artistImg from '../../assets/artist.png';
 const Artists = () => {
   const artists = [
