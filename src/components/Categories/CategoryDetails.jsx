c4f8d9d4ae43989b2f47707939c161a3bcc5e1d4
ezat-elzalouy <ezatelzalouy711@gmail.com>
2021-09-13 17:44:19 +0200

    deploy

--- src/components/Categories/CategoryDetails.jsx
+++ src/components/Categories/CategoryDetails.jsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import _ from "lodash";
-import musician1 from "../../assets/foaG.png";
-=======
 import React , { useEffect }from "react";
 import {Link } from 'react-router-dom';
 import _ from 'lodash';
 import musician1 from '../../assets/foaG.png';
-import music from '../../assets/channels4.jpg'
->>>>>>> 466a9dc92a2bfd72cb515f0d0eb20a61957dcb82
 
 import { useDispatch, useSelector } from "react-redux";
 import { getCategory as httpGetCategory } from "../../httpService/categories";
@@ -131,7 +123,6 @@ const CategoryDetails = (props) => {
                             Brass Sound Band
                           </h5>
                         </div>
-<<<<<<< HEAD
                         <div className="artist-actions mt-1">
                           <svg
                             aria-hidden="true"
@@ -172,47 +163,6 @@ const CategoryDetails = (props) => {
                               </g>
                             </svg>
                           </div>
-=======
-                    </div>
-                </div> 
-                <div class="b-overlay-wrap position-relative">
-                    <div class="row">
-                    {category.artists.map((item, index) => {
-                        return (
-                        <div class="mb-3 col-md-4 col-6" key={index}>
-                            <Link to="/artist" class="category-artist text-decoration-none">
-                                <div class="artist-info">
-                                    <div class="artist-image">
-                                        {item.image ? <img alt=""  src= {`(${ api + item.image })`} class=""/>
-                                        :
-                                        <img alt=""  src={music} class=""/>
-                                        }
-                                        
-                                    </div> 
-                                    <div class="artist-about font-noto-m">
-                                        {_.truncate( item.description,{'length':80,'separator':' '})}
-                                    </div>
-                                </div> 
-                                <div className="artist-name my-2">
-                                    <h5 className="m-0 font-noto text-light">{item.name}</h5>
-                                </div> 
-                                <div className="artist-actions mt-1">
-                                    <svg aria-hidden="true" width="36" height="32" viewBox="0 0 576 512" focusable="false" className="bg-normal p-1 action-icon fa-icon" style={{fontSize:"2.25em"}}>
-                                        <g>
-                                            <path d="M504.7 320h-293.1l6.5 32h268.4c15.4 0 26.8 14.3 23.4 29.3l-5.5 24.3c18.7 9.1 31.6 28.2 31.6 50.4 0 31.2-25.5 56.4-56.8 56-29.8-0.4-54.3-24.6-55.2-54.4-0.4-16.3 6.1-31 16.8-41.5h-209.6c10.4 10.2 16.8 24.3 16.8 40 0 31.8-26.5 57.4-58.7 55.9-28.5-1.3-51.8-24.4-53.3-52.9-1.2-22 10.4-41.5 28.1-51.6l-70.2-343.4h-69.9c-13.3 0-24-10.7-24-24v-16c0-13.3 10.7-24 24-24h102.5c11.4 0 21.2 8 23.5 19.2l9.2 44.8h392.8c15.4 0 26.8 14.3 23.4 29.3l-47.3 208c-2.5 10.9-12.2 18.7-23.4 18.7zM408 168h-48v-40c0-8.8-7.2-16-16-16h-16c-8.8 0-16 7.2-16 16v40h-48c-8.8 0-16 7.2-16 16v16c0 8.8 7.2 16 16 16h48v40c0 8.8 7.2 16 16 16h16c8.8 0 16-7.2 16-16v-40h48c8.8 0 16-7.2 16-16v-16c0-8.8-7.2-16-16-16z"></path>
-                                        </g>
-                                    </svg> 
-                                    <div className="b-overlay-wrap position-relative ml-1" data-toggle="modal" data-target="#auth-required-modal">
-                                        <svg width="1em" height="1em" viewBox="0 0 20 20" focusable="false" role="img" alt="icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi-heart bg-normal p-1 action-icon b-icon bi" style={{fontSize:"225%"}}>
-                                            <g>
-                                                <path fillRule="evenodd" d="M10 4.748l-.717-.737C7.6 2.281 4.514 2.878 3.4 5.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.837-3.362.314-4.385-1.114-2.175-4.2-2.773-5.883-1.043L10 4.748zM10 17C-5.333 6.868 5.279-1.04 9.824 3.143c.06.055.119.112.176.171a3.12 3.12 0 01.176-.17C14.72-1.042 25.333 6.867 10 17z" clipRule="evenodd"></path>
-                                            </g>
-                                        </svg>
-                                    </div>
-                                </div>
-                            </Link>
-                            
->>>>>>> 466a9dc92a2bfd72cb515f0d0eb20a61957dcb82
                         </div>
                       </Link>
                     </div>
