b74328b9fe3bb4e22d5fa3580a1bc46b2927aca5
ezat-elzalouy <ezatelzalouy711@gmail.com>
2021-09-16 12:20:48 +0200

    fix issues

--- src/httpService/categories.js
+++ src/httpService/categories.js
@@ -1,13 +1,6 @@
 import http from "./httpHandler";
 import _try from "../utils/try";
 const route = process.env.REACT_APP_API + "/api/";
-export const getCategories = _try(async () => {
-  const response = await http.get(route + "categories");
-  if (response.status === 200) {
-    return response.data;
-  } else {
-  }
-});
 
 export const getCategories = _try(async () => {
   const response = await http.get(route + "categories");
